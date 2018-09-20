b406d83b2206058ba4d253105e35c658da2404b0
Maxalmina <maxalmina.kahfi@gmail.com>
2018-09-20 10:20:22 +0700

    fix naming issues 2

--- furnish/app/controllers/landing_pages_controller.rb
+++ furnish/app/controllers/landing_pages_controller.rb
@@ -1,4 +1,4 @@
-class LandingCustomerController < ApplicationController
+class LandingPagesController < ApplicationController
     def landing
     end
 end
--- furnish/app/helpers/landing_pages_helper.rb
+++ furnish/app/helpers/landing_pages_helper.rb
@@ -1,2 +1,2 @@
-module LandingCustomerHelper
+module LandingPagesHelper
 end
