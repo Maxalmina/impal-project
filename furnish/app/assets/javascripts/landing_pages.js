981072e974bf47b71a63a121458249b50462f833
amaliahwd <huwaidahamalia@gmail.com>
2018-09-20 14:43:30 +0700

    landing-rev; logo-rev

--- furnish/app/assets/javascripts/landing_pages.js
+++ furnish/app/assets/javascripts/landing_pages.js
@@ -10,3 +10,15 @@ document.addEventListener('DOMContentLoaded', function() {
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+  var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
--- furnish/config/routes.rb
+++ furnish/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/signin", to: "user_sessions#new", as: "signin"
+  get "/index", to: "landing_pages#index", as: "index"
   root "landing_pages#landing"
 end
