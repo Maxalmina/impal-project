81baba4f088e33c04f47c53937964d2d26038c80
amaliahwd <huwaidahamalia@gmail.com>
2018-09-27 14:15:03 +0700

    add landing page

--- app/assets/javascripts/landing_pages.js
+++ app/assets/javascripts/landing_pages.js
@@ -1,24 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, options);
 });
 
-  // Or with jQuery
+// Or with jQuery
 
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-
-  var instance = M.Carousel.init({
-    fullWidth: true,
-    indicators: true
-  });
-
-  // Or with jQuery
-  
-  $('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
-  });
+$(document).ready(function(){
+  $('.slider').slider();
+});
